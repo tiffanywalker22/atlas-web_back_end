f317476371c9c810feea181b712da3fbf9c0b0a1
tiffanywalker22 <tiffanywalker@LAPTOP-5UJ7IJM0>
2024-01-02 19:43:47 -0600

    follow your arrow

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
