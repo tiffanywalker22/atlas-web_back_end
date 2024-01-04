cd649642480c925de0a572e490715a50c410fc9f
Tiffany <tbcasper16@gmail.com>
2024-01-04 17:48:14 -0600

    adding pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+import Currency from "./3-currency";
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(amount) {
+        if (typeof amount !== 'number') {
+            throw TypeError('Amount must be a number');
+        }
+        this._amount = amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(currency) {
+        if (currency instanceof Currency) this._currency = currency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+            throw TypeError('Amount and Conversion Rate must be a number');
+        }
+        return amount * conversionRate;
+    }
+}
