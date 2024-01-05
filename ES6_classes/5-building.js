474134358d805323958f1c0ef4c7adee14b76805
Tiffany <tbcasper16@gmail.com>
2024-01-04 18:46:43 -0600

    finally got building to work

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw TypeError('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+}
