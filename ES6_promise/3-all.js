b450c03c418bb7e26de22bf8422fb812798e3eae
Tiffany <tbcasper16@gmail.com>
2024-01-10 17:37:48 -0600

    working on fixing errors for checker

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto, createUser])
-    .then((response) => {
-      console.log(`${response.body} ${response.firstname} ${response.lastName}`);
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([uploadPhoto, createUser]) => {
+      console.log(`${userResult.body} ${userResult.firstname} ${userResult.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
