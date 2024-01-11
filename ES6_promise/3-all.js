d39d0bfd11411e3c87c0646cbfca2aa34b506512
Tiffany <tbcasper16@gmail.com>
2024-01-10 18:02:26 -0600

    finally got some checks; code actually should be fixed and correct now!

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((userResult) => {
-      console.log(`${userResult.body} ${userResult.firstName} ${userResult.lastName}`);
+    .then(([uploadPhoto, createUser]) => {
+      console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
