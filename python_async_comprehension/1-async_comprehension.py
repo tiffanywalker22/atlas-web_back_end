402afe3b0d05dc8d14383b596e87d6e23c93d610
Tiffany <tbcasper16@gmail.com>
2024-05-02 16:53:39 -0500

    one day i'll remember to pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,7 @@ from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """Import async_generator from the previous task and then write a coroutine
     called async_comprehension that takes no arguments. The coroutine will
