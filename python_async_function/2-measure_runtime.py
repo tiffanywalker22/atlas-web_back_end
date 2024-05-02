b9af2beab22cc4c8cb8d499f9cf86e046926c40c
Tiffany <tbcasper16@gmail.com>
2024-05-02 15:10:54 -0500

    quick fix

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,6 +8,7 @@ from typing import List
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(max_delay: int = 10, n: int = 0) -> float:
     """
     Measures the total execution time for wait_n(n, max_delay)
