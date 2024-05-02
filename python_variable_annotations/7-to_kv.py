af04dfa84e445f731165af58acab0c3936f80471
Tiffany <tbcasper16@gmail.com>
2024-05-02 12:25:18 -0500

    complex types

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""  Returns tuple with string k and the square of int/float v """
+
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns tuple with the string k and the square of int/float v
+
+    Args:
+    - k (str): The string key
+    - v (Union[int, float]): The integer or float value
+
+    Returns:
+    - tuple: A tuple containing k and the square of v as a float
+    """
+    return (k, float(v) ** 2)
