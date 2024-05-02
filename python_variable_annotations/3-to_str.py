0adc674ff67a4a9751a27ef39ef90a03d2267bb6
Tiffany <tbcasper16@gmail.com>
2024-05-02 11:18:51 -0500

    to string or not to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Returns string rep of float """
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string rep of a float
+
+    Args:
+    - n (float): The float number to be converted to a string
+
+    Returns:
+    - str: The string rep of the input float
+    """
+    return str(n)
