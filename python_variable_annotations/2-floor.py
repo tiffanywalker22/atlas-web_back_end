0dcdcd3a1414f8876b4337e54a72e677cef900fa
Tiffany <tbcasper16@gmail.com>
2024-05-02 11:10:46 -0500

    needed more documentation after all

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,4 +3,13 @@
 
 
 def floor(n: float) -> int:
+    """
+    Returns the floor of a float
+
+    Args:
+    - n (float): The float number whose floor is to be calculated
+
+    Returns:
+    - int: The floor of the input float
+    """
     return int(n)
