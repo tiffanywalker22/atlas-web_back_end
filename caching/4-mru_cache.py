ba9c598d4106e3ac7984e8b0e6540d82187eecec
Tiffany <tbcasper16@gmail.com>
2024-05-06 16:17:42 -0500

    mru caching

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
-"""Create a class LRUCache that inherits from BaseCaching
+"""Create a class MRUCache that inherits from BaseCaching
 and is a caching system"""
 
 from base_caching import BaseCaching
 
-
-class LRUCache(BaseCaching):
-    """A caching system that implements the LRU
-    and inherits from BaseCaching
+class MRUCache(BaseCaching):
+    """A caching system that implements the MRU
+    and inherits from BaseCaching.
 
     Attributes:
         cache_data (dict): A dictionary to store cached data
-        usage_order (list): A list to maintain the order
-        of key usage for LRU
+        usage_order (list): A list to maintain the order of key usage for MRU
 
     Methods:
-        put(key, item): Add an item to the cache using LRU
+        put(key, item): Add an item to the cache using MRU
         get(key): Get an item from the cache
     """
 
     def __init__(self):
-        """Initialize the LRUCache"""
+        """Initialize the MRUCache"""
         super().__init__()
         self.usage_order = []
 
     def put(self, key, item):
-        """Add an item to the cache using LRU
+        """Add an item to the cache using MRU
 
         Args:
             key: The key to associate with the item
@@ -35,9 +33,9 @@ class LRUCache(BaseCaching):
             return
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            lru_key = self.usage_order.pop(0)
-            del self.cache_data[lru_key]
-            print(f"DISCARD: {lru_key}")
+            mru_key = self.usage_order.pop()
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
 
         self.cache_data[key] = item
         self.usage_order.append(key)
@@ -54,7 +52,7 @@ class LRUCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
 
-        """ Update the usage order (move the key to the end of the list) """
+        """Update the usage order (move the key to the end of the list)"""
         self.usage_order.remove(key)
         self.usage_order.append(key)
 
