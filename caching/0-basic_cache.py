3aecddaad91e04d76a93cb7e178f18c2be86eae9
Tiffany <tbcasper16@gmail.com>
2024-05-06 14:07:41 -0500

    first commit

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Task 0"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
