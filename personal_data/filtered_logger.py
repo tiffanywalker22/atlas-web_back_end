8f240ddb9209a86a1586e069cce76eaef5a5f9f0
Tiffany <tbcasper16@gmail.com>
2024-05-08 15:20:03 -0500

    fix for variables and return value have been annotated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,9 +2,10 @@
 """Write a function called filter_datum that
 returns the log message obfuscated"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Obfuscates specified fields in a log message
 
