2af9d7389ef07ed32311f1cc9b9f43c37b864c38
Tiffany <tbcasper16@gmail.com>
2024-05-09 21:05:26 -0500

    adding is valid for password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -11,3 +11,7 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+def is_valid(hashed_password, password):
+    """Check if the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
