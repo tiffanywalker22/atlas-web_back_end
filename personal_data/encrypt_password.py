cda0039a52c94601232200694b1554e65e15e733
Tiffany <tbcasper16@gmail.com>
2024-05-09 21:09:14 -0500

    forgot to add correct variables

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -12,6 +12,6 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
-def is_valid(hashed_password, password):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if the provided password matches the hashed password"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
