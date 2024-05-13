da65cc10600d1daee1115936b5e012538494c9b0
Tiffany <tbcasper16@gmail.com>
2024-05-13 14:19:52 -0500

    making a few small fixes

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_endpoint():
-  """endpoint to simulate unauthorized access"""
-  abort(401)
+    """endpoint to simulate unauthorized access"""
+    abort(401)
