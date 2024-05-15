8270de7a565badd498b5f7784ad0ba7f589671ec
Tiffany <tbcasper16@gmail.com>
2024-05-14 20:56:53 -0500

    add user object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 """basic auth class"""
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple, Optional
+from typing import Tuple, Optional, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -52,3 +53,23 @@ class BasicAuth(Auth):
             decoded_base64_authorization_header.split(':', 1)
             )
         return user_email, user_password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> Optional[User]:
+        """
+        Returns the user instance based on email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        user_list = User.search({'email': user_email})
+        if not user_list:
+            return None
+
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
