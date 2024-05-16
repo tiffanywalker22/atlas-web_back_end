5fa7593d6e67a9de4dc7da62fd80bbbe471f57c1
Tiffany <tbcasper16@gmail.com>
2024-05-16 12:20:19 -0500

    revert back to final work now that i got checks back

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -12,11 +12,13 @@ import mysql.connector
 PII_FIELDS = ('name', 'email', 'phone', 'password', 'ssn')
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Connect to the MySQL database using environment variables for credentials
+
     Returns:
     mysql.connector.connection.MySQLConnection: The database connection object
+
     Raises:
     mysql.connector.Error: If there is an error connecting to the database
     """
@@ -42,16 +44,20 @@ def get_db():
 class RedactingFormatter(logging.Formatter):
     """
         Formatter class for redacting sensitive information in log messages
+
         Attributes:
         REDACTION (str): The redacted string to replace sensitive information
         FORMAT (str): The log message format including placeholders
         for logging attributes
         SEPARATOR (str): The separator character used to separate
         fields in log messages
+
         Args:
         fields (List[str]): A list of strings representing fields
         with sensitive information
+
     """
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
@@ -59,55 +65,54 @@ class RedactingFormatter(logging.Formatter):
     def __init__(self, fields: List[str]):
         """
         Initialize the RedactingFormatter
+
         Args:
         fields (List[str]): A list of strings representing fields
         with sensitive information
+
         """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Customizes the format of log messages by
-        obfuscating sensitive information  """
-        def filter_datum(
-                        fields: List[str],
-                        redaction: str,
-                        message: str,
-                        separator: str
-                        ) -> str:
-            """
-            Obfuscates specified fields in a log message
-            Arguments:
-            fields (List[str]): A list of strings representing
-            all fields to obfuscate
-            redaction (str): A string representing by what the field
-            will be obfuscated
-            message (str): A string representing the log line
-            separator (str): A string representing by which character is
-            separating all fields in the log line (message)
-            Returns:
-            str: The obfuscated log message
-            """
-            for field in fields:
-                message = re.sub(f"(?<={field}=).*?(?={separator})",
-                                 redaction, message)
-            return message
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
-                                  self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
 
 
 def get_logger() -> logging.Logger:
     """
     Get a configured logger object for logging user data.
+
     Returns:
         .Logger: The configured logger object.
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
+
     stream_handler = logging.StreamHandler()
     formatter = RedactingFormatter(PII_FIELDS)
     stream_handler.setFormatter(formatter)
+
     logger.addHandler(stream_handler)
+
     return logger
+
+
+def main():
+    """The function will obtain a database connection
+    using get_db and retrieve all rows in the users
+    table and display each row under a filtered format"""
+    db = get_db()
+    logger = get_logger()
+    db_cursor = db.cursor()
+    db_cursor.execute("SELECT * FROM users;")
+    for row in db_cursor:
+        new_row = logger._formatter.format(record=logging.LogRecord
+                                           (name="user_data",
+                                            level=logging.INFO, pathname="",
+                                            lineno=0, msg=row, args=None,
+                                            exc_info=None))
+        logger.info(new_row)
+    db_cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
