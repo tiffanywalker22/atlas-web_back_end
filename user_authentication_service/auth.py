cfe23dcf74d75dd116b77625f97aac4934ffc874
Tiffany <tbcasper16@gmail.com>
2024-05-26 18:26:17 -0500

    moving hash password outside of class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,6 +7,12 @@ from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """Hash a password string using bcrypt and return the hashed bytes"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -20,12 +26,5 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
-            new_user = self._db.add_user(email, hashed_password)
-            return new_user
+            return self._db.add_user(email, _hash_password(password))
 
-    def _hash_password(self, password: str) -> bytes:
-        """Hash a password string using bcrypt and return the hashed bytes"""
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed_password
