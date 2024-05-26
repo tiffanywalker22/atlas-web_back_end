d8b45cb7e0038280971c8fd1822bd39948650f98
Tiffany <tbcasper16@gmail.com>
2024-05-26 18:16:29 -0500

    adding register user and adding self to hash password func

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,10 +2,30 @@
 """ user auth """
 
 import bcrypt
+from user import User
+from db import DB
+from sqlalchemy.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Hash a password string using bcrypt and return the hashed bytes"""
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user with email and password"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hash a password string using bcrypt and return the hashed bytes"""
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
