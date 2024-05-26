4f1c2cefa82c32b5f8c73a5e76f8c37f5553f436
Tiffany <tbcasper16@gmail.com>
2024-05-26 18:27:29 -0500

    pycode fix

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -13,6 +13,7 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -27,4 +28,3 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-
