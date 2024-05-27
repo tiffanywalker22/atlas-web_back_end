928fe16bb576d7c7240be2c4f45552178b36cc3f
Tiffany <tbcasper16@gmail.com>
2024-05-27 13:52:42 -0500

    worked on task one fixes, started task 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """unit tests for utils """
 import unittest
+from unittest.mock import patch, Mock
+from utils import get_json
 from parameterized import parameterized
 from utils import access_nested_map
 
@@ -14,9 +16,30 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b"),
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        with patch('utils.requests.get') as mocked_get:
+            mocked_response = Mock()
+            mocked_response.json.return_value = test_payload
+            mocked_get.return_value = mocked_response
+
+            result = get_json(test_url)
+
+            mocked_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
+
+if __name__ == '__main__':
+    unittest.main()
