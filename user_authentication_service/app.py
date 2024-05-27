546b70d5f488c0e61731b40ed882256ec0798934
Tiffany <tbcasper16@gmail.com>
2024-05-26 21:51:56 -0500

    get reset password token"

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -70,5 +70,19 @@ def profile():
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    email = request.form.get("email")
+
+    if not email:
+        abort(400)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
