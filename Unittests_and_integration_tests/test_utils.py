6fca30324d610e72a62fa7f46bd9b562b2ea6992
Tiffany <tbcasper16@gmail.com>
2024-05-27 14:21:10 -0500

    okay added documentation and quick pycode fix; bye for today!

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -6,13 +6,16 @@ from utils import get_json, memoize
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
+    """tests for access_nested_map function"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """test for accessing values"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -20,16 +23,19 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+        """tests exceptions raised"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
+    """tests for get_json function"""
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(self, test_url, test_payload):
+        """test for fetching json data"""
         with patch('utils.requests.get') as mocked_get:
             mocked_response = Mock()
             mocked_response.json.return_value = test_payload
@@ -40,8 +46,11 @@ class TestGetJson(unittest.TestCase):
             mocked_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
 
+
 class TestMemoize(unittest.TestCase):
+    """tests for memoize"""
     def test_memoize(self):
+        """testing memoization"""
         class TestClass:
             def a_method(self):
                 return 42
@@ -50,7 +59,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mocked_method:
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mocked_method:
             instance = TestClass()
             result1 = instance.a_property
             result2 = instance.a_property
@@ -58,6 +68,7 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
             mocked_method.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
