764fe61e3445b65205b00cd2072b8d11b5f7bfdf
Tiffany <tbcasper16@gmail.com>
2024-05-26 19:26:27 -0500

    didnt need self

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -15,7 +15,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """Generate a new uuid and return its string representation"""
     return str(uuid.uuid4())
 
