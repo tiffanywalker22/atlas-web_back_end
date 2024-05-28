3b527233956be78e0c16b8d0bb741a8479aee688
Tiffany <tbcasper16@gmail.com>
2024-05-28 16:56:02 -0500

    fixing typos and adding test license

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -45,18 +45,29 @@ class TestGithubOrgClient(unittest.TestCase):
         ]
         mock_get_json.return_value = repos_name
         expected_repo = ["repo1"]
-        public_repo_url = "example.com"
+        public_repos_url = "example.com"
 
         with patch.object(GithubOrgClient, '_public_repos_url',
                           new_callable=PropertyMock) as mock_public_repos_url:
-            mock_public_repos_url.return_value = public_repo_url
+            mock_public_repos_url.return_value = public_repos_url
 
             client = GithubOrgClient("test_org")
             result = client.public_repo()
 
             self.assertEqual(result, expected_repo)
             mock_public_repos_url.assert_called_once()
-            mock_get_json.assert_called_once_with(public_repo_url)
+            mock_get_json.assert_called_once_with(public_repos_url)
+
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test GithubOrgClient.has_license returns the correct value"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 
 if __name__ == '__main__':
