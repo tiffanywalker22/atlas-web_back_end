e199579bcde9dacdc89fb195a91e6f95ee55b178
Tiffany <tbcasper16@gmail.com>
2024-05-28 16:18:53 -0500

    Mocking a property

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -25,5 +25,17 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expected_payload)
 
 
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test GithubOrgClient._public_repos_url returns the correct value"""
+        expected_repos_url = "https://api.github.com/orgs/test_org/repos"
+        mock_org.return_value = {"repos_url": expected_repos_url}
+
+        client = GithubOrgClient("test_org")
+        result = client._public_repos_url
+
+        self.assertEqual(result, expected_repos_url)
+
+
 if __name__ == '__main__':
     unittest.main()
