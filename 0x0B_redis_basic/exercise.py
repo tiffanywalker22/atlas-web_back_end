7e4470276b9ca2167378964138321379cb1a5835
Tiffany <tbcasper16@gmail.com>
2024-05-31 17:36:04 -0500

    adding needed documentaion and final pycode fix

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" caching module that uses redis to store and retrieve data """
 import redis
 import uuid
 from typing import Union, Callable, Optional
@@ -6,6 +7,7 @@ from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
+    """counts number of times a method is called"""
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         self._redis.incr(method.__qualname__)
@@ -14,6 +16,7 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
+    """logs the input and output of a method"""
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         input_key = f"{method.__qualname__}:inputs"
@@ -26,30 +29,37 @@ def call_history(method: Callable) -> Callable:
         return output
     return wrapper
 
+
 def replay(method: Callable):
+    """function to replay histotry of i/o for method"""
     input_key = f"{method.__qualname__}:inputs"
     output_key = f"{method.__qualname__}:outputs"
     inputs = method.__self__._redis.lrange(input_key, 0, -1)
     outputs = method.__self__._redis.lrange(output_key, 0, -1)
     print(f"{method.__qualname__} was called {len(inputs)} times:")
     for input_args, output in zip(inputs, outputs):
-        print(f"{method.__qualname__}(*{input_args.decode('utf-8')}) -> {output.decode('utf-8')}")
+        print(f"{method.__qualname__}(*{input_args.decode
+              ('utf-8')}) -> {output.decode('utf-8')}")
 
 
 class Cache:
+    """ class representing cache with redis """
     def __init__(self):
+        """ initalizes cache and makes connection to redis"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """stores data in cache and returns key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
     def get(self, key: str, fn: Optional[Callable]
             = None) -> Union[str, bytes, int, float, None]:
+        """retrieves data from cache based on key"""
         data = self._redis.get(key)
         if data is None:
             return None
@@ -58,7 +68,9 @@ class Cache:
         return data
 
     def get_str(self, key: str) -> Optional[str]:
+        """retrieves string value from cache"""
         return self.get(key, lambda d: d.decode("utf-8"))
 
     def get_int(self, key: bytes) -> int:
+        """retrieves integer value from cache"""
         return self.get(key, lambda d: int(d.decode("utf-8")))
