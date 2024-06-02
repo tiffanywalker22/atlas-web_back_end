83f4eb22e1b4219a9090fd584a069128cce9dadf
Tiffany <tbcasper16@gmail.com>
2024-06-02 17:34:13 -0500

    working on indentation

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -3,6 +3,8 @@
 CREATE TRIGGER after_order_insert
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN UPDATE items
-SET quantity = quantity - NEW.quantity
-WHERE id = NEW.item_id;
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.quantity
+    WHERE id = NEW.item_id
+END;
