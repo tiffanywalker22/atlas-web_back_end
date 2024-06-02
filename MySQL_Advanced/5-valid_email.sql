b08ccf2d6326ea75b37fc44eb0a55a6efb748d4c
Tiffany <tbcasper16@gmail.com>
2024-06-02 18:14:12 -0500

    adding delimiter

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,8 +1,12 @@
 -- task five, email validation to sent
 -- sql script that creates a trigger that resets attribute
+DELIMITER //
 CREATE TRIGGER before_email_update
 BEFORE UPDATE ON users
 FOR EACH ROW
-IF OLD.email <> NEW.email THEN
-SET NEW.valid_email = FALSE
-END IF;
+BEGIN
+    IF OLD.email <> NEW.email THEN
+    SET NEW.valid_email = FALSE;
+    END IF;
+END;
+DELIMITER;
