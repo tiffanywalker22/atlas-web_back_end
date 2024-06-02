e923c011f05174a2df3e3d0c222294b2d1655049
Tiffany <tbcasper16@gmail.com>
2024-06-02 16:49:29 -0500

    glam rock

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- task three, old school band
+-- sql script that lists all bands with glam rock
+SELECT DISTINCT band_name, (IFNULL(SPLIT, 2024)-formed) AS lifespan from metal_bands
+WHERE style LIKE 'Glam rock'
+ORDER BY lifespan DESC;
