637708328d7c8ec6ab14abeef3d660ebf18e27c9
Tiffany <tbcasper16@gmail.com>
2024-06-02 17:41:04 -0500

    ???

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -3,8 +3,6 @@
 CREATE TRIGGER after_order_insert
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE name = NEW.item_nane
-END;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
