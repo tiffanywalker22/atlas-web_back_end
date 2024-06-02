e6590b520c3c79680ca7deb2e2b584f46ef28ca0
Tiffany <tbcasper16@gmail.com>
2024-06-02 17:10:42 -0500

    fixing uppercase mistake and adding % to return correct bands

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- task three, old school band
 -- sql script that lists all bands with glam rock
-SELECT DISTINCT band_name, (IFNULL(SPLIT, 2020)-formed) AS lifespan from metal_bands
-WHERE style LIKE 'Glam rock'
+SELECT DISTINCT band_name, (IFNULL(split, 2020)-formed) AS lifespan from metal_bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
