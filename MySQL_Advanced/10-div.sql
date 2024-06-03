eb627bbe7e3da23ad43a1a4101f0362ea86a87c4
Tiffany <tbcasper16@gmail.com>
2024-06-02 21:06:24 -0500

    safe divide

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- task ten, safe divide
+--
+
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END;
+
+DELIMITER ;
