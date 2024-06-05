7d50d80c4d9cec0d9298c1617b3d0f7c63a44130
Tiffany <tbcasper16@gmail.com>
2024-06-05 02:11:52 -0500

    okay now really last try for tonight

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,15 +3,14 @@
 Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.logs
-collection = db.nginx
-
 
 def nginx_logs_stats():
     """script that provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
     log_count = collection.count_documents({})
-    print(f"{log_count} collections")
+    print(f"{log_count} logs")
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
