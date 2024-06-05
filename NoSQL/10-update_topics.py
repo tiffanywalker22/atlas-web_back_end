70398818eb70984ac4dd13b05f877560c936c060
Tiffany <tbcasper16@gmail.com>
2024-06-04 23:36:03 -0500

    forgot documentation

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,6 +3,8 @@
 document based on the name"""
 
 def update_topics(mongo_collection, name, topics):
+    """function that changes all topics of a school document
+    based on the name"""
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
