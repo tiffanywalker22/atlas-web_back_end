1b4fbe15dda22800bd40664bf1ea47cf7a910b48
Tiffany <tbcasper16@gmail.com>
2024-06-05 02:14:06 -0500

    ???

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,14 +8,14 @@ def nginx_logs_stats():
     """script that provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient()
     db = client.logs
-    collection = db.nginx
-    log_count = collection.count_documents({})
+    logs = db.nginx
+    log_count = logs.count_documents({})
     print(f"{log_count} logs")
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        print(f"\tmethod {method}: {collection.count_documents({'method': method})}")
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+        print(f"\tmethod {method}: {logs.count_documents({'method': method})}")
+    status_check_count = logs.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check_count} status check")
 
 if __name__ == "__main__":
