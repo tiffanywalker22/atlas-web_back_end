3f900fb02d5967f746e0a940ff549cfaa769c8e3
Tiffany <tbcasper16@gmail.com>
2024-06-17 18:56:18 -0500

    chai time

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,29 @@
+// 1-calcul.test.js
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./2-calcul_chai.js');
+
+describe('calculateNumber', function() {
+    describe('SUM', function() {
+        it('should return 6 when inputs are 1.4 and 4.5', function() {
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        });
+    });
+
+    describe('SUBTRACT', function() {
+        it('should return -4 when inputs are 1.4 and 4.5', function() {
+            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+    });
+
+    describe('DIVIDE', function() {
+        it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        });
+
+        it('should return Error when second input rounds to 0', function() {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.2), 'Error');
+        });
+    });
+});
