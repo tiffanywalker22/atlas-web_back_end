02cf5d1e6c4768f15686f64a0bcac1b68821c2e8
Tiffany <tbcasper16@gmail.com>
2024-06-17 19:12:31 -0500

    im silly and forgot to change tests to expect

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -5,24 +5,24 @@ const calculateNumber = require('./2-calcul_chai.js');
 describe('calculateNumber', function() {
     describe('SUM', function() {
         it('should return 6 when inputs are 1.4 and 4.5', function() {
-            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+            expect.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
         });
     });
 
     describe('SUBTRACT', function() {
         it('should return -4 when inputs are 1.4 and 4.5', function() {
-            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+            expect.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
         });
     });
 
     describe('DIVIDE', function() {
         it('should return 0.2 when inputs are 1.4 and 4.5', function() {
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+            expect.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
         });
 
         it('should return Error when second input rounds to 0', function() {
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.2), 'Error');
+            expect.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+            expect.strictEqual(calculateNumber('DIVIDE', 1.4, 0.2), 'Error');
         });
     });
 });
