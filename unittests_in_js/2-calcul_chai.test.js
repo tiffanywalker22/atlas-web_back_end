f976ec5b987c8192429a9d19508017c410e57b48
Tiffany <tbcasper16@gmail.com>
2024-06-19 17:18:26 -0500

    fix and add to.equal

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -5,24 +5,24 @@ const calculateNumber = require('./2-calcul_chai');
 describe('calculateNumber', function() {
     describe('SUM', function() {
         it('should return 6 when inputs are 1.4 and 4.5', function() {
-            expect(calculateNumber('SUM', 1.4, 4.5), 6);
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
         });
     });
 
     describe('SUBTRACT', function() {
         it('should return -4 when inputs are 1.4 and 4.5', function() {
-            expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
         });
     });
 
     describe('DIVIDE', function() {
         it('should return 0.2 when inputs are 1.4 and 4.5', function() {
-            expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
         });
 
         it('should return Error when second input rounds to 0', function() {
-            expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-            expect(calculateNumber('DIVIDE', 1.4, 0.2), 'Error');
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+            expect(calculateNumber('DIVIDE', 1.4, 0.2)).to.equal('Error');
         });
     });
 });
