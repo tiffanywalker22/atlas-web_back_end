3a02d10a7e91a266843bafa91cffd6a4a80077fd
Tiffany <tbcasper16@gmail.com>
2024-06-19 17:25:40 -0500

    misread and copied the correct calc function from task 2

--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -1,8 +1,18 @@
 // utils.js
 const Utils = {
     calculateNumber(type, a, b) {
+        const roundedA = Math.round(a);
+        const roundedB = Math.round(b);
+
         if (type === 'SUM') {
-            return Math.round(a) + Math.round(b);
+            return roundedA + roundedB;
+        } else if (type === 'SUBTRACT') {
+            return roundedA - roundedB;
+        } else if (type === 'DIVIDE') {
+            if (roundedB === 0) {
+                return 'Error';
+            }
+            return roundedA / roundedB;
         }
     }
 };
