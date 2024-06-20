26975838e3ce2ea4060000aa77116becf34dbece
Tiffany <tbcasper16@gmail.com>
2024-06-19 19:36:04 -0500

    revert back and got calcnumsum working

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -4,25 +4,30 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', function() {
+    let calculateNumberStub;
     let consoleLogSpy;
 
     beforeEach(function() {
+        calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
         consoleLogSpy = sinon.spy(console, 'log');
     });
 
     afterEach(function() {
+        calculateNumberStub.restore();
         consoleLogSpy.restore();
     });
 
     it('should call sendPaymentRequestToApi with 100 and 20, and log correct message once', function() {
+        calculateNumberStub.withArgs('SUM', 100, 20).returns(120);
         sendPaymentRequestToApi(100, 20);
+        expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
         expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
-        expect(sendPaymentRequestToApi).to.equal(120);
     });
 
     it('should call sendPaymentRequestToApi with 10 and 10, and log correct message once', function() {
+        calculateNumberStub.withArgs('SUM', 10, 10).returns(20);
         sendPaymentRequestToApi(10, 10);
+        expect(calculateNumberStub.calledOnceWithExactly('SUM', 10, 10)).to.be.true;
         expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
-        expect(sendPaymentRequestToApi).to.equal(20);
     });
 });
