56c1e2d572dab88d8987ef4b457bd3eb7952d2fe
Tiffany <tbcasper16@gmail.com>
2024-06-21 20:32:09 -0500

    ???

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -22,4 +22,16 @@ describe('calculateNumber', function() {
     it('should return 0 when inputs are 0 and 0', function() {
         assert.strictEqual(calculateNumber(0, 0), 0);
     });
+
+    it('should return 4 when second number is rounded and inputs are 1 and 3.2', function() {
+        assert.strictEqual(calculateNumber(1, 3.2), 4);
+    });
+
+    it('should return 6 when both numbers are rounded and inputs are 1.234 and 5.678', function() {
+        assert.strictEqual(calculateNumber(1.234, 5.678), 6);
+    });
+
+    it('should return -2 when second number is rounded and inputs are 1 and -2.5', function() {
+        assert.strictEqual(calculateNumber(1, -2.5), -2);
+    });
 });
