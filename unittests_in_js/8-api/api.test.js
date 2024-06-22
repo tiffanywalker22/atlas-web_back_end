960f5fb8abe7730664b17c848d1c65c9dd6e30cb
Tiffany <tbcasper16@gmail.com>
2024-06-21 21:12:06 -0500

    all tests are working locally now

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,10 +1,10 @@
 const request = require('request');
 const expect = require('chai').expect;
-const app = require('./api');
 
 describe('Index page', function() {
     it('Correct status code?', function(done) {
         request('http://localhost:7865/', function (error, response) {
+            if (error) return done(error);
             expect(response.statusCode).to.equal(200);
             done();
         });
@@ -12,6 +12,7 @@ describe('Index page', function() {
 
     it('Correct result?', function(done) {
         request('http://localhost:7865/', function (error, response, body) {
+            if (error) return done(error);
             expect(body).to.equal('Welcome to the payment system');
             done();
         });
